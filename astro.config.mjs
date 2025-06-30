12074a2dd5dd0cdb1da90f2393021b8f7c49adc4
Ovlove <ovwemue@gmail.com>
2025-06-30 06:30:57 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,20 @@
-// astro.config.mjs
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
 import netlify from '@astrojs/netlify/functions';
+import image from '@astrojs/image';
 
 export default defineConfig({
   output: 'static',
   adapter: netlify(),
-  integrations: [mdx()],
+  integrations: [
+    tailwind({ config: './tailwind.config.mjs' }),
+    mdx(),
+    sitemap(),
+    image(),
+  ],
+  experimental: {
+    assets: true
+  }
 });
