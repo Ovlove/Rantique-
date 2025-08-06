382562fe3d722c9c7faef5fe3528eb94d21d9e53
Ovlove <ovwemue@gmail.com>
2025-08-06 11:20:50 +0100

    Update config.ts

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,11 +1,16 @@
 import { defineCollection, z } from 'astro:content';
 
+const fallbackDate = () => new Date().toISOString();
+
 export const collections = {
   blog: defineCollection({
     schema: z.object({
       title: z.string(),
       description: z.string().optional(),
-      pubDate: z.coerce.date(),
+      pubDate: z
+        .string()
+        .or(z.date())
+        .default(fallbackDate), // fallback if missing
       tags: z.array(z.string()).optional(),
       slug: z.string().optional(),
     }),
@@ -14,7 +19,10 @@ export const collections = {
     schema: z.object({
       title: z.string(),
       description: z.string().optional(),
-      pubDate: z.coerce.date(),
+      pubDate: z
+        .string()
+        .or(z.date())
+        .default(fallbackDate), // fallback if missing
       author: z.string().optional(),
       tags: z.array(z.string()).optional(),
       slug: z.string().optional(),
