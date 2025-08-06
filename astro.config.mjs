fc9b32dbb5e8675fe71eb8f67111ebda02d90630
Ovlove <ovwemue@gmail.com>
2025-08-06 11:49:42 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,17 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import netlify from '@astrojs/netlify';
+import path from 'path';
 
 export default defineConfig({
   output: 'static',
   adapter: netlify(),
   integrations: [mdx()],
+  alias: {
+    '@components': path.resolve('./src/components'),
+    '@layouts': path.resolve('./src/layouts'),
+    '@pages': path.resolve('./src/pages'),
+    '@utils': path.resolve('./src/utils'),
+    '@styles': path.resolve('./src/styles'),
+  },
 });
